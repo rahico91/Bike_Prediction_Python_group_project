49f2505ecb34d5b6bd0f15464b66be811dcd16f7
rahico91 <r.hernandez@student.ie.edu>
2019-05-19 22:38:44 +0200

    Zip

--- Bike-Sharing-Dataset 2/My_Functions.py
+++ Bike-Sharing-Dataset 2/My_Functions.py
@@ -23,9 +23,8 @@ from dask_glm.datasets import make_regression
 
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.pipeline import make_pipeline
+from sklearn.externals import joblib
 
-
-#import dask_searchcv as dscv
 from dask_searchcv import GridSearchCV
 
 from sklearn.ensemble import RandomForestRegressor
@@ -35,91 +34,28 @@ import dask_xgboost as dxgb
 '''-------------------------------------------------------------------------------------------------------------------------'''
 from collections import defaultdict
 
-# from sklearn import preprocessing
-# from sklearn.metrics import mean_squared_error, r2_score,roc_curve
-# from sklearn.model_selection import train_test_split, KFold,StratifiedKFold
-# from sklearn.model_selection import cross_val_score, cross_val_predict,validation_curve
-# from sklearn.ensemble import RandomForestRegressor
-
-
 from sklearn.pipeline import make_pipeline
-# from sklearn.preprocessing import LabelBinarizer, RobustScaler,PolynomialFeatures
-# from sklearn.neighbors import KNeighborsRegressor
-# from scipy import stats
-# from scipy.stats import skew, boxcox_normmax
-# from scipy.special import boxcox1p
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator , MultipleLocator
-#from gplearn.genetic import SymbolicRegressor
-
-# from sklearn import metrics
-# from sklearn.preprocessing import OneHotEncoder, LabelEncoder, LabelBinarizer,MinMaxScaler
-# from sklearn.pipeline import make_pipeline
-# from sklearn.feature_selection import RFE, RFECV
-# from sklearn.linear_model import LogisticRegression,LinearRegression, OrthogonalMatchingPursuit
-# from sklearn.model_selection import train_test_split , TimeSeriesSplit, GridSearchCV
 from sklearn.model_selection import TimeSeriesSplit
-#from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import confusion_matrix, classification_report
-# from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
+
 from matplotlib.gridspec import GridSpec
 import plotly.tools as tls
 import plotly
 import plotly.plotly as py
-# from sklearn.decomposition import PCA
 from pandas import DataFrame 
-# from sklearn.exceptions import ConvergenceWarning
-# from gplearn.genetic import SymbolicTransformer
+
 from scipy.stats import *
 from astral import Astral
 import datetime
 import matplotlib.pyplot as plt
-# from sklearn.metrics import explained_variance_score
 import warnings
-# from sklearn import preprocessing
-# from sklearn.metrics import mean_squared_error, r2_score,roc_curve
-# from sklearn.model_selection import train_test_split, KFold,StratifiedKFold
-# from sklearn.model_selection import cross_val_score, cross_val_predict,validation_curve
-# from sklearn.ensemble import RandomForestRegressor
-# from sklearn.kernel_ridge import KernelRidge
-# from sklearn.svm import SVR
-# import xgboost as xgb
-# from sklearn.linear_model import SGDRegressor
 
 plotly.tools.set_credentials_file(username='Furqan92', api_key='22DfVN5rFRg79OYygN5h')
 
 tscv = TimeSeriesSplit(n_splits=5)
 random_seed = 1234
-
-## Reading data 
-def read_data(input_path):
-    raw_data = pd.read_csv(input_path, keep_default_na=True)
-    return raw_data
-
-
-## Finding the correlation matrix for numerical variables
-def correlation_spear(df):
-    numeric_dtypes = ['int16', 'int32', 'int64',
-                          'float16', 'float32', 'float64']
-    numeric_features = []
-    for i in df.columns:
-        if df[i].dtype in numeric_dtypes:
-            numeric_features.append(i)
-    corr= stats.spearmanr(df[numeric_features])
-    return pd.DataFrame(corr[0], columns=numeric_features,index= numeric_features)
-
-### Dummifying categorical variables
-def onehot_encode(df,category):
-    df = df.copy()
-    numericals = df.get(list(set(df.columns) - set(category)))
-    new_df = numericals.copy()
-    for categorical_column in category:
-        new_df = pd.concat([new_df, 
-                            pd.get_dummies(df[categorical_column], 
-                                           prefix=categorical_column[0])], 
-                           axis=1)
-    return new_df
 
 ## Replacing number in season by real names and in weathersit by description
 def num_name(df):
@@ -131,41 +67,6 @@ def num_name(df):
     df['weathersit']= df.weathersit.apply(
                lambda x: weathersit[x]).astype('category') 
     return df
-
-## fixing desired types
-def fix_types(df):
-    df = df.copy()
-    boolean = ['workingday','weekday','holiday']
-    for j in boolean:
-        df[j]= df[j].astype('int')
-    return df
-
-## Genetic programming function that will create new features
-def Genetic_P(dataset,target):
-    append = 'mean_per_hour'
-    a = dataset[append]
-    y = dataset[target]
-    X=dataset.copy()
-    X=X.drop(target,axis=1)
-    X=X.drop(append,axis =1)
-    function_set = ['add', 'sub', 'mul', 'div',
-                'sqrt', 'log', 'abs', 'neg', 'inv',
-                'max', 'min','sin',
-                 'cos',
-                 'tan']
-    gp = SymbolicTransformer(generations=20, population_size=2000,
-                         hall_of_fame=100, n_components=15,
-                         function_set=function_set,
-                         parsimony_coefficient=0.0005,
-                         max_samples=0.9, verbose=1,
-                         random_state=random_seed, n_jobs=3)
-    gp_features = gp.fit_transform(X,y)
-    print('Number of features created out of genetic programing: {}'.format(gp_features.shape))
-    n = pd.DataFrame(gp_features)
-    n =n.set_index(dataset.index.values)
-    new_X = pd.concat([dataset, n],axis=1)
-    new_X = new_X.dropna()
-    return new_X
 
 ## Creating a new variable that compares the value to the past 7 days 
 ## the first 5 rows will be dropped if 'windspeed'is calculated and only 2 for the rest 
@@ -182,27 +83,6 @@ def relative_values(dataset, columns):
     dataset = dataset.replace([np.inf, -np.inf], np.nan).dropna()
     return dataset 
         
-def check_skewness(df, numerical_cols, p_threshold=(0.75)):
-    skewed_features = list()
-    for feature in numerical_cols:
-        data = df[feature].copy()
-        skewness = skew(data)
-        print("{} skewness p-value : {}".format(feature, skewness))
-
-        if abs(skewness > p_threshold):
-            print(feature)
-            print("SKEWED")
-            skewed_features.append(feature)
-            print("-------------\n")
-    print("\n------\n")
-    print("skewed_features:")
-    print(skewed_features)
-
-    plt.rcParams["figure.figsize"] = (10,5)
-    for i, feature in enumerate(skewed_features):
-        plt.hist(df[feature], bins='auto')
-        plt.title(feature)
-
 # Preperation for isDaylight()
 city_name = 'Washington DC'
 a = Astral()
@@ -210,18 +90,7 @@ a.solar_depression = 'civil'
 city = a[city_name]
 '''Feature Creation Functions'''
 
-### This function will calculate the mean of the cnt of the previous 2 weeks during the same hour
-def mean_per_hour_3weeks(dataset):
-    a = [] 
-    for i in range(0,len(dataset)):
-        a.append(dataset[ (dataset['dteday']>= (dataset['dteday'].iloc[i] + datetime.timedelta(-21))) & ( dataset['dteday'] < (dataset['dteday'].iloc[i])) &( dataset['hr']  == dataset['hr'].iloc[i])]['cnt'].mean())
-    dataset['mean_per_hour']= a
-    dataset= dataset.dropna()
-    return dataset
 
-def r2score(x,y):
-    s = explained_variance_score(x,y)
-    return s 
 
 
 
